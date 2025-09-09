c4168a95f4957e4cb974cfd940aa2de67d7948a3
renatomota5-blip <renatomota5@gmail.com>
2025-09-09 18:59:45 -0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// build.gradle.kts (nível do projeto)
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
