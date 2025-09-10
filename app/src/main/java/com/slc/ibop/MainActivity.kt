c07757e11d8d576d031a29db7dd77d4334ae096d
renatomota5-blip <renatomota5@gmail.com>
2025-09-10 00:30:42 -0300

    Update MainActivity.kt

--- app/src/main/java/com/slc/ibop/MainActivity.kt
+++ app/src/main/java/com/slc/ibop/MainActivity.kt
@@ -1,33 +1,28 @@
 package com.slc.ibop
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity          // ✅ CORRETO
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import com.slc.ibop.data.Prefs
+import com.slc.ibop.ui.HomeScreen
+import com.slc.ibop.ui.ProvisionScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { IboPlusApp() }
-    }
-}
-
-@Composable
-fun IboPlusApp() {
-    MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("IBO Plus funcionando com Compose!")
+        setContent {
+            MaterialTheme {
+                Surface {
+                    var activated by remember { mutableStateOf(Prefs.isActivated(this)) }
+                    if (activated) {
+                        HomeScreen()
+                    } else {
+                        ProvisionScreen(onActivated = { activated = true })
+                    }
+                }
             }
         }
     }
