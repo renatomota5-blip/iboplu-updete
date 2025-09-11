30087af5734a5521452b2483dab5be11360817a7
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 02:06:23 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,21 @@
-rootProject.name = "IBO Plus"
-include ':app'
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// nome do projeto (pode ajustar se quiser mostrar outro nome no Gradle)
+rootProject.name = "iboplu-updete"
+
+// inclui o módulo app (sintaxe correta no Kotlin DSL)
+include(":app")
